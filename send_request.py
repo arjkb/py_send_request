bbe1e9ed33d8ee2c7e3cea1c23ae6c62fcdc5d47
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-09-22 20:00:20 -0600

    accept number of iterations as command-line argument

--- send_request.py
+++ send_request.py
@@ -1,10 +1,10 @@
 import argparse
 import requests
 
-def check_url(url_to_check):
+def check_url(url_to_check, iteration_count):
     blocked_count = 0
     worked_count = 0
-    for i in range(10):
+    for i in range(iteration_count):
         try:
             r = requests.get(url_to_check)
             worked_count += 1
@@ -15,6 +15,9 @@ def check_url(url_to_check):
                                                 worked_count))
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("iter_count", help="number of iterations", type=int)
+    args = parser.parse_args()
 
     urls = ["http://www.whsw.edu.cn/q=freedom",
             "http://www.whsw.edu.cn/q=freenet",
@@ -23,7 +26,7 @@ def main():
             ]
 
     for url in urls:
-        check_url(url)
+        check_url(url, args.iter_count)
 
 
 if __name__ == '__main__':
