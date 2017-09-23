9e941f4842d5f321f9d6e709a4cb155585bc0da5
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-09-22 19:48:04 -0600

    testing with urls hard-coded into a list

--- send_request.py
+++ send_request.py
@@ -2,18 +2,29 @@ import argparse
 import requests
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("url", help="url to send request to", type=str)
-    args = parser.parse_args()
+    ""
+    test_url = "http://www.whsw.edu.cn/?q=freedom"
+    test_url2 = "http://www.nx.gov.cn/q=freenet"
 
-    try:
-        r = requests.get(args.url)
-        print(" Response: {}".format(r.status_code))
-    except Exception as e:
-        print(e)
-        print("Couldn't establish connection...")
+    urls = ["http://www.whsw.edu.cn/q=freedom",
+            "http://www.whsw.edu.cn/q=freenet",
+            "http://www.nx.gov.cn/q=freedom",
+            "http://www.nx.gov.cn/q=freenet",
+            ]
 
-
+    for url in urls:
+        exception_count = 0
+        no_exception_count = 0
+        for i in range(1000):
+            try:
+                r = requests.get(url)
+                # print(" Response: {}".format(r.status_code))
+                no_exception_count += 1
+            except Exception as e:
+                exception_count += 1
+                # print(e)
+                # print(" Couldn't establish connection...")
+        print(" {} Blocked: {}, Not blocked: {}".format(url, exception_count, no_exception_count))
 
 
 if __name__ == '__main__':
